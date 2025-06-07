f197ceaf92cb3cf911c7d2e55bb9ce8d4966bb60
DaryaPrich <pdeprichichik@gmail.com>
2025-06-07 14:47:06 +0300

    Заполнена БД, реализована регистрация и авторизация

--- library_site/main/forms.py
+++ library_site/main/forms.py
@@ -1,8 +1,43 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import CustomUser
+
 
 class RegisterForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Имя пользователя'
+        })
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Email'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Пароль'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Повторите пароль'
+        })
+
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Имя пользователя',
+            'id': 'id_username'
+        })
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Пароль',
+            'id': 'id_password'
+        })
--- library_site/main/views.py
+++ library_site/main/views.py
@@ -1,46 +1,55 @@
-﻿from django.shortcuts import render, get_object_or_404
+﻿from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render, redirect
+
+from .forms import RegisterForm
 from .models import Book
+
 
 def index(request):
     return render(request, 'main/index.html')
+
 
 def books_list(request):
     books = Book.objects.all()
     return render(request, 'main/books_list.html', {'books': books})
 
+
 def book_detail(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     return render(request, 'main/book_detail.html', {'book': book})
 
+
 def account(request):
     return render(request, 'main/account.html')
+
 
 def about(request):
     return render(request, 'main/about.html')
 
+
 def contact(request):
     return render(request, 'main/contact.html')
+
 
 def login_view(request):
     return render(request, 'main/login.html')
 
-def register_view(request):
-    return render(request, 'main/register.html')
-from django.contrib.auth import login, authenticate, logout
-from django.shortcuts import render, redirect
-from .forms import RegisterForm
-from django.contrib.auth.decorators import login_required
 
 def register_view(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)
-            return redirect('account')
+            login(request, user)  # Авторизуем нового пользователя
+            return redirect('account')  # Переход в личный кабинет
     else:
         form = RegisterForm()
     return render(request, 'main/register.html', {'form': form})
+
+
 
 def login_view(request):
     from django.contrib.auth.forms import AuthenticationForm
@@ -54,9 +63,11 @@ def login_view(request):
         form = AuthenticationForm()
     return render(request, 'main/login.html', {'form': form})
 
+
 def logout_view(request):
     logout(request)
     return redirect('index')
+
 
 @login_required
 def account_view(request):
